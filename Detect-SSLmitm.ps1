3d4a21ca0afa42b0d9c8f426ce3406a5fa080815
clr2of8 <clr2of8@gmail.com>
2018-01-26 16:23:44 -0700

    initial commit

---
+++ Detect-SSLmitm.ps1
@@ -0,0 +1,98 @@
+﻿$goldenHashes = @{
+	"www.linkedin.com"  =  "1FB86B1168EC743154062E8C9CC5B171A4B7CCB4";
+	"www.whitehouse.gov"  =  "1FB86B1168EC743154062E8C9CC5B171A4B7CCB4";
+	"www.facebook.com"  =  "A031C46782E6E6C662C2C87C76DA9AA62CCABD8E";
+	"mail.google.com"  =  "A6120FC0B4664FAD0B3B6FFD5F7A33E561DDB87D";
+	"www.google.com"  =  "A6120FC0B4664FAD0B3B6FFD5F7A33E561DDB87D";
+	"www.usbank.com"  =  "CC136695639065FAB47074D28C55314C66077E90";
+	"www.twitter.com"  =  "7E2F3A4F8FE8FA8A5730AECA029696637E986F3F";
+	"www.costco.com"  =  "FF67367C5CD4DE4AE18BCCE1D70FDABD7C866135";
+}
+
+function Detect-SSLmitm {
+
+	foreach ($uri in $goldenHashes.Keys)
+	{
+		$hash = Get-CertHash -Uri "https://$uri"
+		if ($hash -eq $goldenHashes[$uri])
+		{
+			Write-Host -ForegroundColor Green "[*] Certificate hash for $uri matches expected value. No SSL man-in-the-middle detected."
+		}
+		else {
+			Write-Host -ForegroundColor Red "SSL man-in-the-middle detected for $uri"
+		}
+	}
+}
+
+# Get-CertHash function modifed from https://stackoverflow.com/questions/22233702/how-to-download-the-ssl-certificate-from-a-website-using-powershell
+function Get-CertHash
+{
+    PARAM (
+        [Uri]$Uri
+    )
+
+    if (-Not ($uri.Scheme -eq "https"))
+    {
+        Write-Error "You can only get keys for https addresses"
+        return
+    }
+
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $request = [System.Net.HttpWebRequest]::Create($uri)
+	$request.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+    try
+    {
+        #Make the request but ignore (dispose it) the response, since we only care about the service point
+        $request.GetResponse().Dispose()
+    }
+    catch [System.Net.WebException]
+    {
+        if ($_.Exception.Status -eq [System.Net.WebExceptionStatus]::TrustFailure)
+        {
+            #We ignore trust failures, since we only want the certificate, and the service point is still populated at this point
+        }
+        else
+        {
+            #Let other exceptions bubble up, or write-error the exception and return from this method
+            throw
+        }
+    }
+
+    #The ServicePoint object should now contain the Certificate for the site.
+    $servicePoint = $request.ServicePoint
+	# Build the Chain from this certificate so we can pull the secondary (intermediate) CA
+	$chain = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Chain
+	$success = $chain.Build($servicePoint.Certificate)
+	$intermediateCert = $chain.ChainElements[1].Certificate
+	$hash = $intermediateCert.GetCertHashString()
+	return $hash
+	
+}
+
+$Uris = @(
+	"www.google.com"
+	"mail.google.com"
+	"www.whitehouse.gov"
+    "www.costco.com"
+	"www.facebook.com"
+	"www.usbank.com"
+	"www.twitter.com"
+	"www.linkedin.com"
+)
+
+function Get-GoldenHashes
+{
+	$certHashes = @{}
+
+	foreach ($uri in $Uris) {
+        Write-Output "Getting golden certificate hash for $uri"
+		$hash = Get-CertHash -Uri "https://$uri"
+		$certHashes.add($uri, $hash )
+	
+	}
+
+	# print out the hash table for easy copy and paste back into this script as the $goldenHashes variable
+    Write-Host -ForegroundColor Green "[*] Done, now copy and paste these golden hashes into the goldenHashes array at the top of this script."
+	$certHashes.Keys | % { "`t""$_""  =  ""$($certHashes.Item($_))"";" }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Detect-SSLmitm
+
+This PowerShell script will determine if your connection to external servers over HTTPS is being decrypted by an intercepting proxy such as the internet proxies commonly found in corporate environments. It does this by comparing the SSL intermediate certificate being use for your connection to the true/known SSL certificate for the server.
+
+## Usage
+
+Load the PowerShell Module functions from the Windows command prompt as follows:
+
+```
+powershell -exec bypass
+Import-Module .\Detect-SSLmitm.ps1
+```
+
+Determine which sites (in the url list) are being decrypted by an intercepting proxy:
+
+```
+Detect-SSLmitm
+```
+
+The Output looks like this:
+
+![Example Usage](https://github.com/clr2of8/Detect-SSLmitm/raw/master/images/Usage.png)
+
+
+
+If you would like to configure which sites are checked, open the script and edit the "Uris" list near the bottom, then call the following function.
+
+```
+Get-GoldenHashes
+```
+
+Note, it is important to generate the Golden certificate hashes from a network location known to not decrypt SSL traffic, otherwise you will get false positives.
